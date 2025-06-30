79d3a389c0f8fd16a79c9f5819f5d2b3a820205c
Brice Ramdani <ramdani.brice@outlook.com>
2025-06-30 17:45:50 +0200

    Ajout de la route POST /api/auth/signup avec parsing JSON et configuration CORS manuelle

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,23 @@ mongoose.connect(process.env.MONGODB_URI)
 
 
 app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+
+app.post("/api/auth/signup", signUp)
+
+function signUp(req, res, next) {
+    const body = req.body;
+    console.log("body:", body);
+    next();
+}
 
 app.use((req, res, next) => {
     console.log('Requête reçue !');
