33f6aa7e9101d680ae120ba0de7c4ddfc078f8ab
Brice Ramdani <ramdani.brice@outlook.com>
2025-07-07 03:27:37 +0200

    Ajout des routes et controleurs pour la notations

--- backend/controlers/book.js
+++ backend/controlers/book.js
@@ -35,17 +35,17 @@ exports.createBook = (req, res) => {
 exports.modifyBook = (req, res) => {
     const bookObject = req.file ? {
         ...JSON.parse(req.body.book),
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`
     } : { ...req.body };
 
     delete bookObject._userId;
     Book.findOne({ _id: req.params.id })
         .then((book) => {
             if (book.userId != req.auth.userId) {
-                res.status(401).json({ message: 'Not authorized' });
+                res.status(401).json({ message: "Pas autorisé" });
             } else {
                 Book.updateOne({ _id: req.params.id }, { ...bookObject, _id: req.params.id })
-                    .then(() => res.status(200).json({ message: 'Objet modifié!' }))
+                    .then(() => res.status(200).json({ message: "Objet modifié!" }))
                     .catch(error => res.status(401).json({ error }));
             }
         })
@@ -59,15 +59,67 @@ exports.deleteBook = (req, res) => {
     Book.findOne({ _id: req.params.id })
         .then(book => {
             if (book.userId != req.auth.userId) {
-                res.status(401).json({ message: 'Not authorized' });
+                res.status(401).json({ message: "Pas autorisé" });
             } else {
-                const filename = book.imageUrl.split('/images/')[1];
+                const filename = book.imageUrl.split("/images/")[1];
                 fs.unlink(`images/${filename}`, () => {
                     Book.deleteOne({ _id: req.params.id })
-                        .then(() => { res.status(200).json({ message: 'Objet supprimé !' }) })
+                        .then(() => { res.status(200).json({ message: "Objet supprimé !" }) })
                         .catch(error => res.status(401).json({ error }));
                 });
             }
+        })
+        .catch(error => {
+            res.status(500).json({ error });
+        });
+};
+
+exports.createRating = (req, res) => {
+    const userId = req.auth.userId;
+    const rating = req.body.rating;
+
+    if (rating < 0 || rating > 5) {
+        return res.status(400).json({ message: "La note doit être entre 0 et 5" });
+    }
+
+    Book.findById(req.params.id)
+        .then(book => {
+            if (!book) {
+                return res.status(404).json({ message: "Livre introuvable" });
+            }
+
+            const alreadyRated = book.ratings.find(r => r.userId === userId);
+            if (alreadyRated) {
+                return res.status(403).json({ message: "Vous avez déjà noté ce livre" });
+            }
+
+            book.ratings.push({ userId, grade: rating });
+
+            const average = book.ratings.reduce((sum, r) => sum + r.grade, 0) / book.ratings.length;
+            book.averageRating = average;
+
+            return book.save()
+                .then(updatedBook => {
+                    res.status(200).json(updatedBook);
+                });
+        })
+        .catch(error => {
+            res.status(500).json({ error });
+        });
+};
+
+exports.getBestRating = (req, res) => {
+    Book.find()
+        .then(books => {
+            if (!books || books.length === 0) {
+                return res.status(200).json({ message: "Aucun livre trouvé" });
+            }
+
+            const topBooks = books
+                .sort((a, b) => b.averageRating - a.averageRating)
+                .slice(0, 3);
+
+            res.status(200).json(topBooks);
         })
         .catch(error => {
             res.status(500).json({ error });
--- backend/routes/book.js
+++ backend/routes/book.js
@@ -6,12 +6,15 @@ const multer = require('../middlewares/multer-config');
 
 const bookControler = require("../controlers/book");
 
-
+router.get("/bestrating", bookControler.getBestRating);
 router.get("/", bookControler.getAllBooks);
 router.post("/", auth, multer, bookControler.createBook);
 router.get("/:id", bookControler.getOneBook);
 router.put("/:id", auth, multer, bookControler.modifyBook);
 router.delete("/:id", auth, bookControler.deleteBook);
-// router.post("/books/:id/rating")
+router.post("/:id/rating", auth, bookControler.createRating);
+
+
+
 
 module.exports = router;
